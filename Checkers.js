c4d85d2124bade715173a32a5091444b44a9185b
FireRat <27763388+FireRat666@users.noreply.github.com>
2026-01-15 16:45:49 +1100

    update Checkers.js Okay

--- Checkers.js
+++ Checkers.js
@@ -576,9 +576,9 @@
             const pos = getSquarePos(squareId);
             // Tiles are at Y=-0.05, height 0.1, so surface is at Y=0
             if (config.useCustomModels) {
-                pos.y = 0; // Pivot at 0
+                pos.y = 0.1; // Pivot at 0
             } else {
-                pos.y = isKing ? 0.15 : 0.1; // Spheres are centered, so half-height offset
+                pos.y = isKing ? 0.075 : 0.05; // Spheres are centered, so half-height offset
             }
             transform.localPosition = pos;
 
